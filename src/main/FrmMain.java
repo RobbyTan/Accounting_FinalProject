b23db281d26e6b2f7848bb10746b036de1db147e
Phantom <kenichixie@gmail.com>
2017-06-05 20:51:32 +0700

    -init release

--- src/main/FrmMain.java
+++ src/main/FrmMain.java
@@ -1,17 +1,10 @@
 package main;
 
 
-import main.DlgLogin;
 import java.awt.Component;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
